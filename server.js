65a75d4a5ff7a98a9b02c5dc380c005d62f7985e
My Master <derek.straga94@gmail.com>
2020-08-17 15:49:31 -0400

    fixed for deployment

--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
 });
 
 app.use(require("./routes/api.js"));
