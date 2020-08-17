197d928ab4e36cd0fc88c371d8c8bd03ef43e755
My Master <derek.straga94@gmail.com>
2020-08-17 16:06:11 -0400

    fixed for deployment pt 3

--- server.js
+++ server.js
@@ -6,20 +6,15 @@ const PORT = 8000;
 
 const app = express();
 
-var MONGODB_URI = process.env.MONGODB_URL || "mongodb://localhost/workout";
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/workout", {
-  useCreateIndex: true,
   useNewUrlParser: true,
   useFindAndModify: false
 });
-
-
 
 app.use(require("./routes/api.js"));
 app.use(require("./routes/display.js"));
